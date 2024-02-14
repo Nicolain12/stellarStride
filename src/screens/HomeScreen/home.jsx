88f08205e6a9c0abff607ba71b4a5644900636a2
Maxipar03 <maxipar2003@gmial.com>
2024-02-13 21:32:09 -0300

    bottom header

--- src/screens/HomeScreen/home.jsx
+++ src/screens/HomeScreen/home.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
-import { BannerHome } from '../../components/HomeSections/BannerHome/bannerHome'
-import "./home.css"
-import { ServiceHome } from '../../components/HomeSections/ServicesHome/serviceHome'
-import { PlansHome } from '../../components/HomeSections/PlansHome/plansHome'
-
+import React from 'react';
+import { Link, Element } from 'react-scroll';
+import { BannerHome } from '../../components/HomeSections/BannerHome/bannerHome';
+import "./home.css";
+import { ServiceHome } from '../../components/HomeSections/ServicesHome/serviceHome';
+import { PlansHome } from '../../components/HomeSections/PlansHome/plansHome';
 
 function Home() {
-
   return (
     <div className='homeComponent'>
-    <BannerHome />
-    <ServiceHome />
-    <PlansHome />
+      <Element name="bannerHome">
+        <BannerHome />
+      </Element>
+      <Element name="serviceHome">
+        <ServiceHome />
+      </Element>
+      <Element name="plansHome">
+        <PlansHome />
+      </Element>
+  
+      <nav className='navSection'>
+        <Link className='navLink' to="bannerHome" smooth={true} duration={500}>Banner</Link>
+        <Link className='navLink' to="serviceHome" smooth={true} duration={500}>Service</Link>
+        <Link className='navLink' to="plansHome" smooth={true} duration={500}>Plans</Link>
+      </nav>
     </div>
-  )
+  );
 }
 
-export {Home}
+export { Home };
