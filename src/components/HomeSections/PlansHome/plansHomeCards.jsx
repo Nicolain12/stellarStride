7452cc6225a778d7ae1f729ea0a20a1e59cd12ab
Nicolain12 <lainnicolas1@gmail.com>
2024-02-21 21:23:22 -0300

    popup plans first version

--- src/components/HomeSections/PlansHome/plansHomeCards.jsx
+++ src/components/HomeSections/PlansHome/plansHomeCards.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { color, motion } from "framer-motion";
 import { PopupPlans } from "./popupPlans";
 import "./plansHomeCards.css";
+import marketingPopup from "../../../public/images/popupPlans/marketingPopup.png"
 
 function PlansHomeCards() {
   const [selectedPlan, setSelectedPlan] = useState(null);
@@ -12,44 +13,87 @@ function PlansHomeCards() {
 
   const closePopup = () => {
     setSelectedPlan(null);
-  };  
-  
-  
-  const plans = [{ name: "Marketing", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },
-    { name: "E-Commerce", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },
-    { name: "AudioVisual", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },
-    { name: "Web App", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },
-    { name: "Total", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },
-    { name: "Customized", description: "Web site creation with maintenance in time and marketing sistem includeing accounts management", service: "Web Development - Data Analityc - Marketing - Business Management", startingPrice: 800, monthPrice: 650 },]
+  };
 
-    return (
-        <div className="homePlansDiv">
-          {plans.map((plan, index) => (
-            <div className="homePlans" key={index}>
-              <div className="homePlansInfoDiv">
-                <h2 className="plansHomeName">{plan.name}</h2>
-                <h4 className="plansHomeServiceIncludes">Services Included: {plan.service}</h4>
+
+  const plans = [
+    {
+      name: "Marketing",
+      img: marketingPopup,
+      description: "Digital marketing can profoundly benefit businesses by expanding their reach, increasing brand visibility, driving website traffic, generating leads, facilitating customer engagement, boosting sales, and fostering long-term customer relationships. Digital marketing can profoundly benefit businesses by expanding their reach, increasing brand visibility, driving website traffic, generating leads, facilitating customer engagement, boosting sales, and fostering long-term customer relationships.Digital marketing can profoundly benefit businesses by expanding their reach, increasing brand visibility, driving website traffic, generating leads, facilitating customer engagement, boosting sales, and fostering long-term customer relationships.",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },
+    {
+      name: "E-Commerce",
+      img: '',
+      description: "Web site creation with maintenance in time and marketing sistem includeing accounts management",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },
+    {
+      name: "AudioVisual",
+      img: '',
+      description: "Web site creation with maintenance in time and marketing sistem includeing accounts management",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },
+    {
+      name: "Web App",
+      img: '',
+      description: "Web site creation with maintenance in time and marketing sistem includeing accounts management",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },
+    {
+      name: "Total",
+      img: '',
+      description: "Web site creation with maintenance in time and marketing sistem includeing accounts management",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },
+    {
+      name: "Customized",
+      img: '',
+      description: "Web site creation with maintenance in time and marketing sistem includeing accounts management",
+      service: "Web Development - Data Analityc - Marketing - Business Management",
+      startingPrice: 800,
+      monthPrice: 650
+    },]
+
+  return (
+    <div className="homePlansDiv">
+      {plans.map((plan, index) => (
+        <div className="homePlans" key={index}>
+          <div className="homePlansInfoDiv">
+            <h2 className="plansHomeName">{plan.name}</h2>
+            <h4 className="plansHomeServiceIncludes">Services Included: {plan.service}</h4>
+          </div>
+          <div className="homePlansInfMeetoDiv">
+            <div className="homePlansInfoPriceDiv">
+              <div className="plansStartingPrice">
+                <h4 className="plansStartingPriceTitle">Starting Price</h4>
+                <h3 className="plansStartingPriceNumber">${plan.startingPrice}</h3>
               </div>
-              <div className="homePlansInfMeetoDiv">
-                <div className="homePlansInfoPriceDiv">
-                  <div className="plansStartingPrice">
-                    <h4 className="plansStartingPriceTitle">Starting Price</h4>
-                    <h3 className="plansStartingPriceNumber">${plan.startingPrice}</h3>
-                  </div>
-                  <div className="plansMonthPrice">
-                    <h4 className="plansStartingPriceTitle">Month Price</h4>
-                    <h3 className="plansStartingPriceNumber">${plan.monthPrice}</h3>
-                  </div>
-                </div>
-                <motion.button   whileHover={{ backgroundColor: "black", transition: { duration: 0.3 } }} className="plansLinkMeet" onClick={() => openPopup(plan)}>
-                  Learn More
-                </motion.button>
+              <div className="plansMonthPrice">
+                <h4 className="plansStartingPriceTitle">Month Price</h4>
+                <h3 className="plansStartingPriceNumber">${plan.monthPrice}</h3>
               </div>
             </div>
-          ))}
-          {selectedPlan && <PopupPlans plan={selectedPlan} onClose={closePopup} />}
+            <motion.button whileHover={{ backgroundColor: "black", transition: { duration: 0.3 } }} className="plansLinkMeet" onClick={() => openPopup(plan)}>
+              Learn More
+            </motion.button>
+          </div>
         </div>
-      );
-    }
-    
-    export { PlansHomeCards };
+      ))}
+      {selectedPlan && <PopupPlans plan={selectedPlan} onClose={closePopup} />}
+    </div>
+  );
+}
+
+export { PlansHomeCards };
--- src/components/HomeSections/PlansHome/popupPlans.jsx
+++ src/components/HomeSections/PlansHome/popupPlans.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./popupPlans.css";
 
 const PopupPlans = ({ plan, onClose }) => {
+  console.log(plan.img)
+  const schedule = 'Schedule A Meet >>>'
   return (
     <div className="popup">
       <div className="popup-content">
-        <h2>{plan.name}</h2>
-        <p>{plan.description}</p>
-        <button onClick={onClose}>Close</button>
+        <div className="top-popup-div">
+          <h2 className="popup-title">{plan.name}</h2>
+          <button className="popup-close-button" onClick={onClose}>x</button>
+        </div>
+        <div className="middle-popup-div">
+          <img className="popup-img" src={plan.img} alt="" />
+          <p className="popup-description">{plan.description}</p>
+        </div>
+        <div className="bottom-popup-div">
+          <p className="popup-schedule-button">{schedule}</p>
+        </div>
       </div>
     </div>
   );
 };
 
-export {PopupPlans};
+export { PopupPlans };
